f476ec40b1887f16e642127aefc328ad216c774e
Aidan Pentecost <pent0028@algonquinlive.com>
2023-12-03 15:54:28 -0500

    Implemented Comic class
    --Committed by Aidan

--- app/src/main/java/com/cst2335/finalproject/Comic.java
+++ app/src/main/java/com/cst2335/finalproject/Comic.java
@@ -1,11 +1,16 @@
 package com.cst2335.finalproject;
 
 public class Comic {
+    private String title;
+    private String imageUrl;
+
+    public Comic(String title, String imageUrl){
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
     public String getTitle() {
-        return null;
+        return title;
     }
 
-    public String getImageUrl() {
-        return null;
-    }
+    public String getImageUrl() {return imageUrl;}
 }

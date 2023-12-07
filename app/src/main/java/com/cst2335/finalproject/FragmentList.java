0f222b7321668a96d9ed8b01d5fa1f335e3587a4
Aidan Pentecost <pent0028@algonquinlive.com>
2023-12-06 23:25:39 -0500

    Removed unnecessary files
    --Committed by Aidan

--- app/src/main/java/com/cst2335/finalproject/FragmentList.java
+++ app/src/main/java/com/cst2335/finalproject/FragmentList.java
@@ -11,36 +11,8 @@ import android.view.ViewGroup;
 
 public class FragmentList extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public FragmentList() {
         // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static FragmentList newInstance(String param1, String param2) {
-        FragmentList fragment = new FragmentList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
--- app/src/main/java/com/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/com/cst2335/finalproject/MainActivity.java
@@ -1,27 +1,15 @@
 package com.cst2335.finalproject;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-
 import org.w3c.dom.Entity;
-
-import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
- private List<Entity> comicList;
+ private List<Entity> entityList;
 
 // TODO: add code so activity main can function
      @Override

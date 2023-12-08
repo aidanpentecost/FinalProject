cd05589a969fa2b917d0cade414f15822b1fb2b6
Ryan <rthompso2004@gmail.com>
2023-12-07 19:29:21 -0500

    Got AlertDialog for Manga button

--- app/src/main/java/com/cst2335/finalproject/BrowseActivity.java
+++ app/src/main/java/com/cst2335/finalproject/BrowseActivity.java
@@ -1,15 +1,21 @@
 package com.cst2335.finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -28,6 +34,8 @@ public class BrowseActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragmentContainerView2, fragmentList, "Comic List");
         transaction.commit();
+
+
 
         drawerLayout = findViewById(R.id.drawerLayout);
         NavigationView navigationView = findViewById(R.id.navigationView);
@@ -58,6 +66,32 @@ public class BrowseActivity extends AppCompatActivity {
                 }
             }
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                handleBottomNavigationItemClick(item);
+                return false;
+            }
+        });
+
+    }
+
+    private void handleBottomNavigationItemClick(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.Manga) {
+            showMangaAlertDialog();
+        }
+    }
+
+    private void showMangaAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Go Back To Comics")
+                .setMessage("We Don't Read Manga, You Are A Dweeb")
+                .setPositiveButton("OK", null)
+                .show();
     }
 
     private void showSnackbar(String message) {
